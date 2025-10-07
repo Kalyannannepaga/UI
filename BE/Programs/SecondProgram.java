211b730c4be7b5f3ff7b669f0047769e9ed82870
Kalyan Nannepaga <kalyannannepaga@Kalyans-MacBook-Air.local>
2025-10-08 00:09:23 +0530

    Added SecondProgram

---
+++ BE/Programs/SecondProgram.java
@@ -0,0 +1,21 @@
+/**
+ * The type Second program.
+ */
+class SecondProgram {
+    /**
+     * Instantiates a new Get details.
+     */
+    public void getDetails(){
+         System.out.println("Hello kalyan");
+     }
+
+    /**
+     * Instantiates a new Main.
+     *
+     * @param a the a
+     */
+    public  void main (String a[]){
+        getDetails();
+    }
+
+}
